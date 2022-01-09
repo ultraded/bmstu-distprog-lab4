d0f552112262b7b51b4cfade6ceb4035249ba022
ultraded <andrey.chernenkov@mail.ru>
2022-01-09 23:47:49 +0300

    Scripted auto-commit on change (2022-01-09 23:47:49) by gitwatch.sh

--- src/main/java/Server.java
+++ src/main/java/Server.java
@@ -1,5 +1,6 @@
 package main.java;
 
+import akka.NotUsed;
 import akka.actor.ActorSystem;
 import akka.http.javadsl.ConnectHttp;
 import akka.http.javadsl.Http;
@@ -7,16 +8,18 @@ import akka.http.javadsl.ServerBinding;
 import akka.http.javadsl.model.HttpRequest;
 import akka.http.javadsl.model.HttpResponse;
 import akka.stream.ActorMaterializer;
+import akka.stream.javadsl.Flow;
 
+import java.io.IOException;
 import java.util.concurrent.CompletionStage;
 
 public class Server {
     public static void main(String[] args) throws IOException {
         System.out.println("start!");
-        ActorSystem system = ActorSystem.create("routes");
-        final Http http = Http.get(system);
-        final ActorMaterializer materializer = ActorMaterializer.create(system);
-        final Flow<HttpRequest, HttpResponse, NotUsed> routeFlow = <вызов метода которому передаем Http, ActorSystem и ActorMaterializer>;
+        ActorSystem actorSystem = ActorSystem.create("routes");
+        final Http http = Http.get(actorSystem);
+        final ActorMaterializer materializer = ActorMaterializer.create(actorSystem);
+        final Flow<HttpRequest, HttpResponse, NotUsed> routeFlow = createFlow()
         final CompletionStage<ServerBinding> binding = http.bindAndHandle(
                 routeFlow,
                 ConnectHttp.toHost("localhost", 8080),
